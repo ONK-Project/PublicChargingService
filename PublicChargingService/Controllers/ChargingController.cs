cd7f089f782f07ceb99ba4ff32ff04af1efc2dfd
Harfeld <andreas.harfeld@gmail.com>
2020-04-17 12:44:50 +0200

    Add project files.

---
+++ PublicChargingService/Controllers/ChargingController.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using Models;
+
+namespace PublicChargingService.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class ChargingController : ControllerBase
+    {
+        private readonly ILogger<ChargingController> _logger;
+
+        public ChargingController(ILogger<ChargingController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        [Route("heat")]
+        public PriceAndTaxes getHeat()
+        {
+            PriceAndTaxes pt = new PriceAndTaxes();
+            pt.Currency = "Dkk";
+            pt.Price = 222.34;
+            pt.Tax = 0.45;
+            pt.UnitOfMeassure = "kWh";
+
+            return pt;
+        }
+
+        [HttpGet]
+        [Route("water")]
+        public PriceAndTaxes getWater()
+        {
+            return null;
+        }
+
+        [HttpGet]
+        [Route("electricity")]
+        public PriceAndTaxes getElectricity()
+        {
+            return null;
+        }
+    }
+}
---
+++ PublicChargingService/Models/PriceAndTaxes.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Models
+{
+    public class PriceAndTaxes
+    {
+        public double Price { get; set; }
+        public string Currency { get; set; }
+        public string UnitOfMeassure { get; set; }
+        public double Tax { get; set; }
+    }
+}
